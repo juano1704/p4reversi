e3bd52ab65cd4d38da552be7cb18854750493809
borjalopez91 <borjalopez91@hotmail.com>
2012-04-20 08:57:50 +0000

    format

--- reversi/src/clases/Partida.java
+++ reversi/src/clases/Partida.java
@@ -3,32 +3,31 @@ package clases;
 public class Partida {
 	private String nombre;
 
-	
-	public Partida()
-	{
-		nombre="";
-		
+	public Partida() {
+		nombre = "";
+
 	}
-	public Partida(String n)
-	{
-		nombre=n;
+
+	public Partida(String n) {
+		nombre = n;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
-	public void mostrar()
-	{
+
+	public void mostrar() {
 		System.out.println("Nombre de la partida:" + nombre);
 	}
-	public static void main(String []args)
-	{
+
+	public static void main(String[] args) {
 		System.out.println("Introduce el nombre de la partida:");
-		String nombre=Utilidades.leerCadena();
-		Partida p=new Partida();
+		String nombre = Utilidades.leerCadena();
+		Partida p = new Partida();
 		p.mostrar();
 	}
 }
