540c6bf8f9966d4a23f2a19163d5079ce532d387
borjalopez91 <borjalopez91@hotmail.com>
2012-04-24 11:09:02 +0000

    panel en JUEGO

--- reversi/src/ventanas/juego.java
+++ reversi/src/ventanas/juego.java
@@ -5,182 +5,248 @@ import java.awt.event.*;
 import java.util.logging.*;
 import javax.swing.*;
 
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
 public class juego extends JFrame implements ActionListener {
 
-public juego() {
-initComponents();
-}
+	public juego() {
+		initComponents();
+	}
 
-ImageIcon blanca = new ImageIcon();
-ImageIcon negra = new ImageIcon();
-ImageIcon tablero = new ImageIcon();
+	ImageIcon blanca = new ImageIcon();
+	ImageIcon negra = new ImageIcon();
+	ImageIcon tablero = new ImageIcon();
 
-@SuppressWarnings("unchecked")
-private void initComponents() {
+	@SuppressWarnings("unchecked")
+	private void initComponents() {
 
-blanca = new ImageIcon("src\\imagenes\\blanca.jpg");
-negra = new ImageIcon("src\\imagenes\\negra.jpg");
-tablero = new ImageIcon("src\\imagenes\\tablero.jpg");
+		blanca = new ImageIcon("src\\imagenes\\blanca.jpg");
+		negra = new ImageIcon("src\\imagenes\\negra.jpg");
+		tablero = new ImageIcon("src\\imagenes\\tablero.jpg");
 
-botonOpciones = new JButton();
+		botonOpciones = new JButton();
 
-jLabel1 = new JLabel(); // imagen del tablero en juego
-jLabel2 = new JLabel();
-jLabel3 = new JLabel();
-jLabel4 = new JLabel();
-jLabel5 = new JLabel();
-jLabel6 = new JLabel();
-jLabel7 = new JLabel();
+		jLabel1 = new JLabel(); // imagen del tablero en juego
+		jLabel2 = new JLabel();
+		jLabel3 = new JLabel();
+		jLabel4 = new JLabel();
+		jLabel5 = new JLabel();
+		jLabel6 = new JLabel();
+		jLabel7 = new JLabel();
+		jPanel1 = new JPanel();
 
-botonOpciones.setText("Opciones");
+		botonOpciones.setText("Opciones");
 
-jLabel1.setIcon(tablero);
-jLabel2.setText("Jugador 1:");
-jLabel3.setText("Jugador 2:");
-jLabel4.setIcon(blanca);
-jLabel5.setIcon(negra);
-jLabel6.setText("NUM");
-jLabel7.setText("NUM");
+		jLabel1.setIcon(tablero);
+		jLabel2.setText("Jugador 1:");
+		jLabel3.setText("Jugador 2:");
+		jLabel4.setIcon(blanca);
+		jLabel5.setIcon(negra);
+		jLabel6.setText("NUM");
+		jLabel7.setText("NUM");
 
-this.setTitle("Tabla de Juego");
-this.setResizable(false);
-setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setTitle("Tabla de Juego");
+		this.setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-getContentPane().setLayout(layout);
-layout.setHorizontalGroup(
-layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-.addGroup(layout.createSequentialGroup()
-.addGap(25, 25, 25)
-.addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 361, javax.swing.GroupLayout.PREFERRED_SIZE)
-.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
-.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-.addComponent(botonOpciones)
-.addComponent(jLabel3)
-.addGroup(layout.createSequentialGroup()
-.addComponent(jLabel4)
-.addGap(18, 18, 18)
-.addComponent(jLabel6))
-.addGroup(layout.createSequentialGroup()
-.addComponent(jLabel5)
-.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-.addComponent(jLabel7))
-.addComponent(jLabel2))
-.addGap(21, 21, 21))
-);
-layout.setVerticalGroup(
-layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-.addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-.addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-.addComponent(jLabel2)
-.addGap(18, 18, 18)
-.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-.addComponent(jLabel4)
-.addComponent(jLabel6))
-.addGap(55, 55, 55)
-.addComponent(jLabel3)
-.addGap(18, 18, 18)
-.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-.addComponent(jLabel5)
-.addComponent(jLabel7))
-.addGap(62, 62, 62)
-.addComponent(botonOpciones, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-.addGap(63, 63, 63))
-.addGroup(layout.createSequentialGroup()
-.addGap(27, 27, 27)
-.addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 332, javax.swing.GroupLayout.PREFERRED_SIZE)
-.addContainerGap(37, Short.MAX_VALUE))
-);
+		javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(
+				jPanel1);
+		jPanel1.setLayout(jPanel1Layout);
+		jPanel1Layout
+				.setHorizontalGroup(jPanel1Layout
+						.createParallelGroup(
+								javax.swing.GroupLayout.Alignment.LEADING)
+						.addGroup(
+								javax.swing.GroupLayout.Alignment.TRAILING,
+								jPanel1Layout
+										.createSequentialGroup()
+										.addContainerGap(41, Short.MAX_VALUE)
+										.addGroup(
+												jPanel1Layout
+														.createParallelGroup(
+																javax.swing.GroupLayout.Alignment.LEADING)
+														.addComponent(botonOpciones)
+														.addComponent(jLabel3)
+														.addGroup(
+																jPanel1Layout
+																		.createSequentialGroup()
+																		.addComponent(
+																				jLabel4)
+																		.addGap(18,
+																				18,
+																				18)
+																		.addComponent(
+																				jLabel6))
+														.addGroup(
+																jPanel1Layout
+																		.createSequentialGroup()
+																		.addComponent(
+																				jLabel5)
+																		.addPreferredGap(
+																				javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+																		.addComponent(
+																				jLabel7))
+														.addComponent(jLabel2))
+										.addGap(38, 38, 38)));
+		jPanel1Layout
+				.setVerticalGroup(jPanel1Layout
+						.createParallelGroup(
+								javax.swing.GroupLayout.Alignment.LEADING)
+						.addGroup(
+								jPanel1Layout
+										.createSequentialGroup()
+										.addGap(39, 39, 39)
+										.addComponent(jLabel2)
+										.addGap(18, 18, 18)
+										.addGroup(
+												jPanel1Layout
+														.createParallelGroup(
+																javax.swing.GroupLayout.Alignment.BASELINE)
+														.addComponent(jLabel4)
+														.addComponent(jLabel6))
+										.addGap(55, 55, 55)
+										.addComponent(jLabel3)
+										.addGap(18, 18, 18)
+										.addGroup(
+												jPanel1Layout
+														.createParallelGroup(
+																javax.swing.GroupLayout.Alignment.BASELINE)
+														.addComponent(jLabel5)
+														.addComponent(jLabel7))
+										.addGap(62, 62, 62)
+										.addComponent(
+												botonOpciones,
+												javax.swing.GroupLayout.PREFERRED_SIZE,
+												44,
+												javax.swing.GroupLayout.PREFERRED_SIZE)
+										.addContainerGap(
+												javax.swing.GroupLayout.DEFAULT_SIZE,
+												Short.MAX_VALUE)));
 
-botonOpciones.addActionListener(this);
+		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(
+				getContentPane());
+		getContentPane().setLayout(layout);
+		layout.setHorizontalGroup(layout.createParallelGroup(
+				javax.swing.GroupLayout.Alignment.LEADING).addGroup(
+				layout.createSequentialGroup()
+						.addGap(25, 25, 25)
+						.addComponent(jLabel1,
+								javax.swing.GroupLayout.PREFERRED_SIZE, 361,
+								javax.swing.GroupLayout.PREFERRED_SIZE)
+						.addGap(30, 30, 30)
+						.addComponent(jPanel1,
+								javax.swing.GroupLayout.PREFERRED_SIZE,
+								javax.swing.GroupLayout.DEFAULT_SIZE,
+								javax.swing.GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE,
+								Short.MAX_VALUE)));
+		layout.setVerticalGroup(layout
+				.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+				.addGroup(
+						layout.createSequentialGroup()
+								.addGap(27, 27, 27)
+								.addGroup(
+										layout.createParallelGroup(
+												javax.swing.GroupLayout.Alignment.LEADING,
+												false)
+												.addComponent(
+														jPanel1,
+														javax.swing.GroupLayout.DEFAULT_SIZE,
+														javax.swing.GroupLayout.DEFAULT_SIZE,
+														Short.MAX_VALUE)
+												.addComponent(
+														jLabel1,
+														javax.swing.GroupLayout.DEFAULT_SIZE,
+														332, Short.MAX_VALUE))
+								.addContainerGap(37, Short.MAX_VALUE)));
 
-pack();
-this.setLocationRelativeTo(null);
-}
+		botonOpciones.addActionListener(this);
 
-public void actionPerformed(ActionEvent e) {
-JButton pulsado = (JButton) e.getSource();
-if (pulsado == botonOpciones) 
-opciones();
-}
+		pack();
+		this.setLocationRelativeTo(null);
+	}
 
-public void opciones(){
-int messageType = JOptionPane.QUESTION_MESSAGE;
-String[] options = {"Reiniciar", "Guardar", "Menú Principal", "Cancelar"};
-int code = JOptionPane.showOptionDialog(null, 
-" OPCIONES", 
-"Opciones", 0, messageType, 
-null, options, "Cancelar");
-if(code==0)//Reiniciar
-{
-this.dispose();
-new juego().setVisible(true);
-}
-if(code==1)
-{
-//guardar
-}
-if(code==3)//Cancelar
-{
+	public void actionPerformed(ActionEvent e) {
+		JButton pulsado = (JButton) e.getSource();
+		if (pulsado == botonOpciones)
+			opciones();
+	}
 
-}
-if(code==2)//Menu Principal
-{
-int codigo=JOptionPane.showConfirmDialog(null, "¿Seguro que quiere salir?");
-if(codigo==0){
-this.dispose();
-new menuPrincipal().setVisible(true);
-}
-if(codigo==1||codigo==2){
-opciones();
-}
-}
-}
-public static void main(String args[]) {
+	public void opciones() {
+		int messageType = JOptionPane.QUESTION_MESSAGE;
+		String[] options = { "Reiniciar", "Guardar", "Menú Principal",
+				"Cancelar" };
+		int code = JOptionPane.showOptionDialog(null, " OPCIONES", "Opciones",
+				0, messageType, null, options, "Cancelar");
+		if (code == 0)// Reiniciar
+		{
+			this.dispose();
+			new juego().setVisible(true);
+		}
+		if (code == 1) {
+			// guardar
+		}
+		if (code == 3)// Cancelar
+		{
 
-try {
-for (UIManager.LookAndFeelInfo info : UIManager
-.getInstalledLookAndFeels()) {
-if ("Nimbus".equals(info.getName())) {
-UIManager.setLookAndFeel(info.getClassName());
-break;
-}
-}
-} catch (ClassNotFoundException ex) {
-java.util.logging.Logger.getLogger(juego.class.getName()).log(
-java.util.logging.Level.SEVERE, null, ex);
-} catch (InstantiationException ex) {
-java.util.logging.Logger.getLogger(juego.class.getName()).log(
-java.util.logging.Level.SEVERE, null, ex);
-} catch (IllegalAccessException ex) {
-java.util.logging.Logger.getLogger(juego.class.getName()).log(
-java.util.logging.Level.SEVERE, null, ex);
-} catch (UnsupportedLookAndFeelException ex) {
-java.util.logging.Logger.getLogger(juego.class.getName()).log(
-java.util.logging.Level.SEVERE, null, ex);
-}
+		}
+		if (code == 2)// Menu Principal
+		{
+			int codigo = JOptionPane.showConfirmDialog(null,
+					"¿Seguro que quiere salir?");
+			if (codigo == 0) {
+				this.dispose();
+				new menuPrincipal().setVisible(true);
+			}
+			if (codigo == 1 || codigo == 2) {
+				opciones();
+			}
+		}
+	}
 
-java.awt.EventQueue.invokeLater(new Runnable() {
+	public static void main(String args[]) {
 
-public void run() {
-new juego().setVisible(true);
-}
-});
-}
+		try {
+			for (UIManager.LookAndFeelInfo info : UIManager
+					.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (ClassNotFoundException ex) {
+			java.util.logging.Logger.getLogger(juego.class.getName()).log(
+					java.util.logging.Level.SEVERE, null, ex);
+		} catch (InstantiationException ex) {
+			java.util.logging.Logger.getLogger(juego.class.getName()).log(
+					java.util.logging.Level.SEVERE, null, ex);
+		} catch (IllegalAccessException ex) {
+			java.util.logging.Logger.getLogger(juego.class.getName()).log(
+					java.util.logging.Level.SEVERE, null, ex);
+		} catch (UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(juego.class.getName()).log(
+					java.util.logging.Level.SEVERE, null, ex);
+		}
 
-// Declaracion de variables
-private JButton botonOpciones;
-private JLabel jLabel1;
-private JLabel jLabel2;
-private JLabel jLabel3;
-private JLabel jLabel4;
-private JLabel jLabel5;
-private JLabel jLabel6;
-private JLabel jLabel7;
+		java.awt.EventQueue.invokeLater(new Runnable() {
+
+			public void run() {
+				new juego().setVisible(true);
+			}
+		});
+	}
+
+	// Declaracion de variables
+	private JButton botonOpciones;
+	private JLabel jLabel1;
+	private JLabel jLabel2;
+	private JLabel jLabel3;
+	private JLabel jLabel4;
+	private JLabel jLabel5;
+	private JLabel jLabel6;
+	private JLabel jLabel7;
+	private JPanel jPanel1;
 
 }
