903d7fc8f3adfd29ca16a804fdbaf922d9d24297
anderaginako <anderaginako@gmail.com>
2012-05-02 11:59:16 +0000

    a ver si ahora...

--- reversi/src/ventanas/preguntarRegistro.java
+++ reversi/src/ventanas/preguntarRegistro.java
@@ -1,11 +1,13 @@
 package ventanas;
 
-import interfaz.GUI;
+import otelo.interfaz.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+
+import otelo.interfaz.OteloGUI;
 
 
 public class preguntarRegistro extends javax.swing.JFrame implements
@@ -100,7 +102,7 @@ public class preguntarRegistro extends javax.swing.JFrame implements
 		} else if (pulsado == botonModoInvitado)// Modo invitado
 		{
 
-			new GUI();
+			new OteloGUI();
 			this.dispose();
 		}
 	}
